700c51259c3503c414ec11888b00a89748e31b70
Cole Smith <uniquecolesmith@gmail.com>
2016-04-09 15:52:45 +0800

    + finish task-18

---
+++ src/js/task-18.js
@@ -0,0 +1,60 @@
+;(function () {
+  "use strict";
+
+  var data = [10, 3, 7, 12, 11, 30];
+
+  // 更新数据
+  var update = function (className) {
+    if (! className) return;
+
+    switch(className) {
+      case 'left-in':
+        var value = document.getElementById('user-input').value.trim();
+        if (! value) return ;
+        data.unshift(value);
+        break;
+
+      case 'right-in':
+        var value = document.getElementById('user-input').value.trim();
+        if (! value) return ;
+        data.push(value);
+        break;
+ 
+      case 'left-out':
+        if (data.length === 0) {alert("已经没有值可以出了！"); return ;}
+        data.shift();
+        break;
+
+      case 'right-out':
+        if (data.length === 0) {alert("已经没有值可以出了！"); return ;}
+        data.pop();
+        break;
+    }
+  };
+
+  // 渲染数据
+  var render = function () {
+    document.getElementById("show-area").innerHTML = data.map(function (v1) {
+      return `<span>${v1}</span>`;
+    }).join("");
+  };
+
+  // 事件代理
+  var delegate = function () {
+    Array.prototype.forEach.call(document.getElementsByTagName("button"), function (item) {
+      item.onclick = function (e) {
+          e = e || window.event;
+          var el = e.srcElement || e.target;
+          update(el.className);
+          render();
+      };
+    });
+  };
+
+  function init () {
+    render();
+    delegate();
+  }
+
+  init();
+})();
